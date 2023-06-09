a8866681403429df3006b4d87ada3aeef43dff33
Lucas Silva <lucasemanoel295@gmail.com>
2023-06-09 12:01:32 -0300

    User entity and resource

---
+++ src/main/java/com/devlucassilva/springProject/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.devlucassilva.springProject.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devlucassilva.springProject.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
