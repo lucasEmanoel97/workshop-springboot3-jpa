0b32af181da2a03f86e65e0f957c0ff220103edf
Lucas Silva <lucasemanoel295@gmail.com>
2023-06-11 16:28:19 -0300

    Service layer, component registration

---
+++ src/main/java/com/devlucassilva/springProject/services/UserService.java
@@ -0,0 +1,26 @@
+package com.devlucassilva.springProject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devlucassilva.springProject.entities.User;
+import com.devlucassilva.springProject.repositories.UserRepository;
+
+@Service //registrado como componente de independencia
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
