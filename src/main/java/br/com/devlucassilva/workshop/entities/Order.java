400d71adb2ebf4783eb2416c6d237c7b1f9e10a8
Lucas Silva <lucasemanoel295@gmail.com>
2023-09-12 12:08:49 -0300

    OrderStatus enum

--- src/main/java/br/com/devlucassilva/workshop/entities/Order.java
+++ src/main/java/br/com/devlucassilva/workshop/entities/Order.java
@@ -1,5 +1,6 @@
 package br.com.devlucassilva.workshop.entities;
 
+import br.com.devlucassilva.workshop.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -23,6 +24,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id") // nome da chave estrangeira
     private User client;
@@ -30,9 +33,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +54,15 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null)
+            this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
