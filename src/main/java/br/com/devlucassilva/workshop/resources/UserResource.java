931d3f92ffa9d3fa4f9d9a31ba764f29314b9af8
Lucas Silva <lucasemanoel295@gmail.com>
2023-09-16 20:38:17 -0300

    User delete

--- src/main/java/br/com/devlucassilva/workshop/resources/UserResource.java
+++ src/main/java/br/com/devlucassilva/workshop/resources/UserResource.java
@@ -35,4 +35,10 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
